3296c8e62594f3be3ccbefa0c3fdce7333028ac6
reversio(pizza) <1248142.231.23>
2019-04-13 10:59:11 +0300

    added user schema

---
+++ js/UserSchema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true, required: [true, "no blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+  email: {type: String, lowercase: true, unique: true, required: [true, "no blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  password: String,
+}, {timestamps: true});
+
+
+UserSchema.plugin(uniqueValidator, {message: 'already exists.'});
+
+mongoose.model('User', UserSchema);
+
