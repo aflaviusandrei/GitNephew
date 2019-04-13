e3e4f08c2e8cc4f10c2d1d4797994e5957bac3cb
aflaviusandrei <aflaviusandrei@gmail.com>
2019-04-13 12:48:35 +0300

    made responsive main page

--- frontend/frontendjs/index.js
+++ frontend/frontendjs/index.js
@@ -1,15 +1,39 @@
-var tryButton = document.getElementsByClassName("try-button")[0];
-var regArea = document.getElementById("getstarted");
-var closeReg = document.getElementsByClassName("close-reg")[0];
+var tryButton = document.getElementsByClassName("try-button")[0],
+    regArea = document.getElementById("getstarted"),
+    closeReg = document.getElementById("close-reg");
 
-tryButton.onclick = function() {
-
+function toggleReg() {
     regArea.classList.toggle("open-reg");
-
 }
 
-closeReg.onclick = function() {
+tryButton.onclick = toggleReg;
+closeReg.onclick = toggleReg;
 
-    regArea.classList.toggle("open-reg");
+function toggleNav() {
+    nav.classList.toggle("open-nav");
+}
 
+var menuBut = document.getElementsByClassName("menu-but")[0],
+    nav = document.getElementById("navigation"),
+    closeNav = document.getElementById("close-nav");
+
+menuBut.onclick = toggleNav;
+closeNav.onclick = toggleNav;
+
+var regOpts = document.getElementsByClassName("reg-opt"),
+    login = document.getElementById("login"),
+    register = document.getElementById("register");
+
+regOpts[0].onclick = function() {
+    login.classList.add("picked-option");
+    register.classList.remove("picked-option");
+    document.getElementsByClassName("highlight")[0].classList.remove("highlight");
+    this.classList.add("highlight");
+}
+
+regOpts[1].onclick = function() {
+    login.classList.remove("picked-option");
+    register.classList.add("picked-option");
+    document.getElementsByClassName("highlight")[0].classList.remove("highlight");
+    this.classList.add("highlight");
 }
